72e6ba944e63de05d4ea2f3c0e22b459d42f3127
Mitch Allen <github@mitchallen.com>
2022-11-06 15:52:20 -0500

    feat(pen-turtle): started building out pen-turtle

--- packages/pen-turtle/__tests__/pen-turtle.test.js
+++ packages/pen-turtle/__tests__/pen-turtle.test.js
@@ -7,8 +7,43 @@ const factory = require('..');
 describe('pen-turtle', function () {
   context('smoke test', function () {
     it('should return object', function (done) {
-      let pen = factory();
-      assert.deepEqual(pen,{});
+      let pen = factory.create();
+      assert.ok(pen, 'pen should not be null');
+      done();
+    });
+    it('should have getColor method', function (done) {
+      let TEST_DEFAULT_COLOR = 0x000000;
+      let pen = factory.create();
+      let color = pen.getColor();
+      assert.equal(color, TEST_DEFAULT_COLOR, 'default getColor method return not what expected');
+      done();
+    });
+    it('should have getPath method', function (done) {
+      let TEST_DEFAULT_PATH = [];
+      let pen = factory.create();
+      let path = pen.getPath();
+      assert.deepEqual(path, TEST_DEFAULT_PATH, 'default getPath method return not what expected');
+      done();
+    });
+    it('should have isDown method', function (done) {
+      let TEST_DEFAULT_IS_DOWN = false;
+      let pen = factory.create();
+      let flag = pen.isDown();
+      assert.equal(flag, TEST_DEFAULT_IS_DOWN, 'default isDown method return not what expected');
+      done();
+    });
+    it('should have down method', function (done) {
+      let pen = factory.create();
+      pen.down();
+      let flag = pen.isDown();
+      assert.equal(flag, true, 'down method should cause isDown to be true');
+      done();
+    });
+    it('should have up method', function (done) {
+      let pen = factory.create();
+      pen.up();
+      let flag = pen.isDown();
+      assert.equal(flag, false, 'up method should cause isDown to be false');
       done();
     });
   });
--- packages/pen-turtle/lib/pen-turtle.js
+++ packages/pen-turtle/lib/pen-turtle.js
@@ -1,7 +1,26 @@
 'use strict';
 
-module.exports = penTurtle;
+module.exports.create = function( spec = {} ) {
 
-function penTurtle() {
-    return {};
+    let {
+        color = 0x000000,
+    } = spec;
+
+    let _down = false,
+        _path = [];
+        
+    return {
+
+        getColor: () => color,
+        getPath: () => _path,
+        isDown: () => _down,
+        down: () => { 
+            _down = true;
+            return this;
+        },
+        up: () => { 
+            _down = false;
+            return this;
+        },
+    };
 }
